21ee7ddbf9bc9373a8ae7c222958aa5f6585f683
EvgeniyGuzenkov <jack199479@gmail.com>
2020-10-05 17:30:08 +0300

    refactor: move actions to Cipher class

--- src/Cipher.ts
+++ src/Cipher.ts
@@ -1,7 +1,6 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-underscore-dangle */
 import { Transform, TransformCallback, TransformOptions } from 'stream';
-import actions from './actions';
 
 export type TAction = 'encode' | 'decode';
 
@@ -19,7 +18,7 @@ class Cipher extends Transform {
 
   _transform(chunk: any, encode: BufferEncoding, callback: TransformCallback) {
     try {
-      const action = this.action === 'encode' ? actions.encode : actions.decode;
+      const action = this.action === 'encode' ? this.encode : this.decode;
       const handledChunk = action(
         chunk.toString('utf8'),
         Math.round(this.shift)
@@ -30,6 +29,41 @@ class Cipher extends Transform {
       callback(err);
     }
   }
+
+  private encode = (input: string, shift: number): string => {
+    return input.split('').map(this.getShiftedChar(shift)).join('');
+  };
+
+  private decode = (input: string, shift: number): string =>
+    this.encode(input, -shift);
+
+  private getShiftedCode = (code: number, shift: number): number => {
+    const FIRST_UPPER_CHAR_CODE = 65;
+    const FIRST_LOWER_CHAR_CODE = 97;
+    const CHAR_COUNT = 26;
+    const firstCharCode =
+      code >= FIRST_LOWER_CHAR_CODE
+        ? FIRST_LOWER_CHAR_CODE
+        : FIRST_UPPER_CHAR_CODE;
+
+    return (
+      firstCharCode +
+      ((CHAR_COUNT + (code - firstCharCode) + shift) % CHAR_COUNT)
+    );
+  };
+
+  private isLetter = (char: string) => /[a-zA-Z]/.test(char);
+
+  private getShiftedChar = (shift: number) => (char: string): string => {
+    if (!this.isLetter(char)) {
+      return char;
+    }
+
+    const code = char.codePointAt(0) as number;
+    const shiftedCode = this.getShiftedCode(code, shift);
+
+    return String.fromCodePoint(shiftedCode);
+  };
 }
 
 export default Cipher;
